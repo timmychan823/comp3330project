243743ea73064a1a3106a6a288cdd4b6e36c5433
timmychan823 <chanshunhei09@gmail.com>
2023-11-20 19:03:13 +0800

    Enlarged image done (just needa replace it with the real image later)

--- app/src/main/java/com/example/ricelegfinder/restaurant_cym.kt
+++ app/src/main/java/com/example/ricelegfinder/restaurant_cym.kt
@@ -3,8 +3,11 @@ package com.example.ricelegfinder
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 import androidx.activity.ComponentActivity
 import androidx.appcompat.widget.AppCompatImageView
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class restaurant_cym : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +15,9 @@ class restaurant_cym : ComponentActivity() {
         setContentView(R.layout.activity_restaurant_cym)
 
         val imageback_cymcanteen: AppCompatImageView = findViewById(R.id.imageBack_cymcanteen)
+        val imageView: ImageView = findViewById(R.id.imageView)
+        val enlargedView: ImageView = findViewById(R.id.enlarged_imageView)
+        val wholeLayout: ConstraintLayout = findViewById(R.id.wholeLayout)
 
         imageback_cymcanteen.setOnClickListener{
 
@@ -19,6 +25,20 @@ class restaurant_cym : ComponentActivity() {
                 intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT)
                 startActivityIfNeeded(intent,0)
             }
+        imageView.setOnClickListener{
+            imageView.visibility = View.GONE
+            enlargedView.visibility = View.VISIBLE
+            wholeLayout.alpha = 0.5F
+
         }
+        enlargedView.setOnClickListener{
+            imageView.visibility = View.VISIBLE
+            enlargedView.visibility = View.GONE
+            wholeLayout.alpha = 1F
+        }
+
+        }
+
+
 
 }
